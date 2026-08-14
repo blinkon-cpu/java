a6c11a471d2fadd3415be14f1355535cfb5c1c42
blinkon-cpu <blinkon@gmail.com>
2026-08-14 20:44:43 -0300

    Update Nota.java

--- Nota.java
+++ Nota.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Nota {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-
+        entrada.useLocale(Locale.US);
         System.out.print("Digite a nota do aluno: ");
         float nota = entrada.nextFloat();
 
